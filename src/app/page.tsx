ac8154273bd127b5c512c0ca554270dfb0778dd2
erik-ferreira <erikdesouzagf@gmail.com>
2023-08-02 23:40:38 -0300

    feat: dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-slate-900 p-8 text-slate-100">
-      <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
-        Hello guys
-      </h1>
-      <h2>Hello tailwind</h2>
-      <p>Hello paragraph</p>
+    <div className="h-screen bg-slate-50 p-8 text-slate-900 flex flex-col items-center text-center dark:bg-slate-900 dark:text-slate-100">
+      <div className="max-w-2xl">
+        <h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl">
+          Rapidly build modern websites without ever leaving your HTML.
+        </h1>
+        <p className="mt-4 dark:text-slate-400">
+          A utility-first CSS framework packed with classes like flex, pt-4,
+          text-center and rotate-90 that can be composed to build any design,
+          directly in your markup.
+        </p>
 
-      <button
-        disabled
-        className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
-      >
-        Sign in
-      </button>
+        <button className="bg-sky-500 text-white px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed dark:bg-sky-400 dark:text-sky-950">
+          Sign in
+        </button>
+      </div>
     </div>
   )
 }
